52baab2063744330b8e001957e7793283dc72e71
tuanteng96 <tuantengyn@gmail.com>
2024-12-21 16:08:56 +0700

    update

--- calendar-admin/src/App/modules/Calendar/CalendarPage.js
+++ calendar-admin/src/App/modules/Calendar/CalendarPage.js
@@ -534,6 +534,7 @@ function CalendarPage(props) {
       RootIdS: values.RootIdS
         ? values.RootIdS.map((item) => item.value).toString()
         : "",
+      Roots: values.RootIdS,
       UserServiceIDs:
         values.UserServiceIDs && values.UserServiceIDs.length > 0
           ? values.UserServiceIDs.map((item) => item.value).toString()
@@ -664,6 +665,7 @@ function CalendarPage(props) {
       ...values,
       MemberID: values.MemberID.value,
       RootIdS: values.RootIdS.map((item) => item.value).toString(),
+      Roots: values.RootIdS,
       UserServiceIDs:
         values.UserServiceIDs && values.UserServiceIDs.length > 0
           ? values.UserServiceIDs.map((item) => item.value).toString()
@@ -784,6 +786,7 @@ function CalendarPage(props) {
           ...values,
           MemberID: values.MemberID.value,
           RootIdS: values.RootIdS.map((item) => item.value).toString(),
+          Roots: values.RootIdS,
           UserServiceIDs:
             values.UserServiceIDs && values.UserServiceIDs.length > 0
               ? values.UserServiceIDs.map((item) => item.value).toString()
