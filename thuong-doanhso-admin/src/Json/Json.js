10273455d5daf0392b4198e5065ae6a62e9698e6
tuanteng96 <tuantengyn@gmail.com>
2025-02-10 18:02:02 +0700

    update

--- thuong-doanhso-admin/src/Json/Json.js
+++ thuong-doanhso-admin/src/Json/Json.js
@@ -1,9 +1,9 @@
 export const TypeStaff = window.top?.GlobalConfig?.Admin?.hoa_hong_tu_van_ktv_an
-  ? [{ value: "TU_VAN", label: "Hoa hồng tư vấn" }]
+  ? [{ value: "TU_VAN", label: window.top?.GlobalConfig?.Admin?.hoa_hong_tu_van || "Hoa hồng tư vấn" }]
   : [
-      { value: "TU_VAN", label: "Hoa hồng tư vấn" },
+      { value: "TU_VAN", label: window.top?.GlobalConfig?.Admin?.hoa_hong_tu_van || "Hoa hồng tư vấn" },
       {
         value: "KY_THUAT_VIEN",
-        label: "Hoa hồng tư vấn ( KH mới )",
+        label: window.top?.GlobalConfig?.Admin?.hoa_hong_tu_van_khm || "Hoa hồng tư vấn ( KH mới )",
       },
     ];
